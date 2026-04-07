305c8319e8ffb40cb28c8f144eddcb3b9f0ec898
Awesome Hippo <215847605+AwesomeHippo@users.noreply.github.com>
2026-04-07 16:44:08 +0200

    first stuff

--- src/main/java/tfcuwcompat/TFCUWCompat.java
+++ src/main/java/tfcuwcompat/TFCUWCompat.java
@@ -1,5 +1,9 @@
 package tfcuwcompat;
 
+import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import tfcuwcompat.api.logger.Logger;
 
@@ -9,5 +13,21 @@ public class TFCUWCompat {
     public static final String modID = "tfcuwcompat";
     public static final Logger logger = new Logger(modID);
 
-    public TFCUWCompat() {}
+    public TFCUWCompat() {
+        MinecraftForge.EVENT_BUS.register(this);
+        logger.info(""); // idk what you put when you
+    }
+
+    @SubscribeEvent
+    public void onBiomeLoading(BiomeLoadingEvent event) {
+        // remove all Untamed Wilds spawns
+        for (MobCategory category : MobCategory.values()) {
+            event.getSpawns().getSpawnerData(category).removeIf(spawnerData -> {
+                if (spawnerData.type != null && spawnerData.type.getRegistryName() != null) {
+                    return "untamedwilds".equals(spawnerData.type.getRegistryName().getNamespace());
+                }
+                return false;
+            });
+        }
+    }
 }
