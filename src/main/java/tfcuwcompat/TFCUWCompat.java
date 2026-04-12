441d0fd823dbbeae7de4e657acbb4af7725a56ff
Thenemex <tnmx777@gmail.com>
2026-04-12 23:03:25 +0200

    Added some loggers

--- src/main/java/tfcuwcompat/TFCUWCompat.java
+++ src/main/java/tfcuwcompat/TFCUWCompat.java
@@ -3,6 +3,7 @@ package tfcuwcompat;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.MobSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,13 +17,28 @@ public class TFCUWCompat {
 
     public TFCUWCompat() {
         MinecraftForge.EVENT_BUS.register(this);
+        logger.info("Loading mod ...");
     }
 
+    /*
     @SubscribeEvent
     public static void onFinalizeSpawn(MobSpawnEvent.FinalizeSpawn event) {
         ResourceLocation id = EntityType.getKey(event.getEntity().getType());
+        logger.info("Spawning :", id.getNamespace(), id.getPath());
         if (id.getNamespace().equals("untamedwilds")) {
             event.setSpawnCancelled(true);
+            logger.info("Cancelled :", id.getNamespace(), id.getPath());
+        }
+    }
+    */
+
+    @SubscribeEvent
+    public static void onEntityJoin(EntityJoinLevelEvent event) {
+        ResourceLocation id = EntityType.getKey(event.getEntity().getType());
+        logger.info("Spawning :", id.getNamespace(), id.getPath());
+        if (id.getNamespace().equals("untamedwilds")) {
+            event.setCanceled(true);
+            logger.info("Cancelled :", id.getNamespace(), id.getPath());
         }
     }
     
